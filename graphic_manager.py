8130c5c96b0ab0062bafa31e884f69cae641d83e
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-10 23:04:47 -0600

    Descubri como documentar clases XD, aplicación de Docstrings para una mayor comunicación en equipo

--- graphic_manager.py
+++ graphic_manager.py
@@ -1,11 +1,22 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+import warnings #Para hacer advertencias en el código
 
 class Window(tk.Tk):
+    # DOCSTRING: https://www.datacamp.com/tutorial/docstrings-python
     def __init__(self, title='ventana', geometry:tuple[int, int]=(500,500), icon_image='', **kwargs):
+        """
+        Crea una ventana, usar como si fuese el tipico root = tk.Tk()
+        Por defecto muestra un frame en blanco, pero se le puede asignar uno
+        usando set_principal_page()
+
+        :param title: Título de la ventana
+        :param geometry: Geometria en pixeles de la ventana
+        :param icon_image: Si existe, coloca una imagen de icono
+        :param kwargs: Hereda parametros de la clase tk.TK()
+        """
         super().__init__(**kwargs)
         self.__principal_page = None
-        self.__CURRENT_FRAME = None
+        self.__current_frame = None
         self.__desactive_exit = False
         # ----------------------------------WINDOWS-----------------------------------
         self.title(title)
@@ -20,13 +31,19 @@ class Window(tk.Tk):
             self.iconphoto(True, icon_image)
 
     def set_principal_page(self, page):
+        """
+        Deja como Frame o página principal el frame que se desee.
+        Este frame aparecerá cuando se ejecute el programa
+
+        :param page: El frame que se desea dejar como principal(predefinido)
+        """
         self.__principal_page = page
         self.change_frame(self.__principal_page)
 
     def change_frame(self, page):
-        self.__CURRENT_FRAME = page
-        self.__CURRENT_FRAME.grid(row=0, column=0, sticky='nsew')
-        self.__CURRENT_FRAME.tkraise()
+        self.__current_frame = page
+        self.__current_frame.grid(row=0, column=0, sticky='nsew')
+        self.__current_frame.tkraise()
 
     def exit_root(self):
         if not self.__desactive_exit: self.quit()
@@ -39,11 +56,22 @@ class Window(tk.Tk):
     def desactive_exit(self, value: bool):
         self.__desactive_exit = value
 
+    @property
+    def current_frame(self): return self.__current_frame
+
 
 class Page(tk.Frame):
     # **kwargs para heredar todos los argumentos de tk.Frame
     # https://www.geeksforgeeks.org/python/packing-and-unpacking-arguments-in-python
     def __init__(self, master:Window, **kwargs):
+        """
+        Crea una página, las páginas son frames que permiten organizar en secciones
+        widgets para un uso específico. Se adaptan al tamaño de la pantalla y por medio
+        de change_page() se puede saltar a otra página y los widgets que se deseen pueden
+        ser limpiados en este salto de página.
+        :param master: La ventana donde se colocará la página, debe ser de tipo Window
+        :param kwargs: cualquier otro atributo de tk.Frame()
+        """
         super().__init__(master=master, **kwargs)
         self._clear_widgest = []
 
@@ -55,68 +83,58 @@ class Page(tk.Frame):
     def clear_widgest(self, widgets: list[tk.Widget]):
         self.clear_widgest.extend(widgets)
 
-    def __clear_widgets_in_frame(self):
+    def _clear_widgets_in_frame(self):
         for widget in self._clear_widgest:
-            if isinstance(widget, tk.Label):
-                widget.config(text=' ')
-            elif isinstance(widget, tk.Entry):
-                widget.delete(0, tk.END)
-            elif isinstance(widget, tk.Checkbutton):
-                widget.deselect()
-            elif isinstance(widget, tk.Radiobutton):
-                widget.deselect()
-            elif isinstance(widget, tk.Listbox):
-                widget.selection_clear(0, tk.END)
+            if isinstance(widget, tk.Label): widget.config(text=' ')
+            elif isinstance(widget, tk.Entry): widget.delete(0, tk.END)
+            elif isinstance(widget, tk.Checkbutton): widget.deselect()
+            elif isinstance(widget, tk.Radiobutton): widget.deselect()
+            elif isinstance(widget, tk.Listbox): widget.selection_clear(0, tk.END)
 
     def change_page(self, page):
-        if self._clear_widgest: self.__clear_widgets_in_frame()
-        self.master.change_frame(page)
-
+        """
+        Cambia de este frame a otro, verifica que el frame desde el que cambias es el actual
+        :param page: El frame al que se quiere transportar
+        """
+        if self.master.current_frame == self:
+            if self._clear_widgest: self._clear_widgets_in_frame()
+            self.master.change_frame(page)
+        else: raise SyntaxError("Estas realizando un cambio de página desde un frame que no es el actual")
 
 class PagePrincipal(Page):
     def __init__(self, master, **kwargs):
         super().__init__(master=master, **kwargs)
         master.set_principal_page(self)
 
-
 class ScrollFrame(tk.Frame):
-    def __init__(self, master, vbar_position='right', hbar_position='bottom', **kwargs):
+    def __init__(self, master, vbar_position="right", hbar_position='bottom', **kwargs):
         super().__init__(master=master, **kwargs)
         if int(self.cget('width')) > 0 and int(self.cget('height')) > 0: self.pack_propagate(False)
-        self.__canvas_scroll = tk.Canvas(self, bg= '#ffffff')
+        self.__canvas_scroll = tk.Canvas(self, bg= '#5ECFFF')
         self.__canvas_scroll.pack_propagate(False)
         # BARRAS PARA MOVER (SE ME OLVIDO COMO SE DICEN EN ESPAÑOL xd)
         self.__vbar = tk.Scrollbar(self, orient="vertical")  # VERTICAL BAR
-
         self.__vbar.config(command=self.__canvas_scroll.yview)
         self.__hbar = tk.Scrollbar(self, orient="horizontal")  # HORIZONTAL BAR
-
         self.__hbar.config(command=self.__canvas_scroll.xview)
-        # HACER QUE EL CANVAS SE MUEVA
+        #Configura el vbar y hbar para alinearlos dentro de este frame y se ajusta el comando que se llama cuando se actualiza el scroll
         self.__canvas_scroll.config(xscrollcommand=self.__hbar.set, yscrollcommand=self.__vbar.set)
-
         self.__vbar.pack(side=vbar_position, fill="y")
         self.__hbar.pack(side=hbar_position, fill="x")
 
         self.__canvas_scroll.pack(side="left", expand=True, fill='both')
-        self.__sub_frame = tk.Frame(self.__canvas_scroll)
-        # ESTO LO GUARDO NOMAS PARA LIMITAR EL SCROLL (PCHES BOTONES)
+        self.__sub_frame = tk.Frame(self.__canvas_scroll, width=self.cget('width'), height=self.cget('height'))
+        self.__sub_frame.pack_propagate(False)
         self.__canvas_scroll.create_window((0, 0), window=self.__sub_frame, anchor="nw")
-        #self.__sub_frame.bind("<Configure>",
-        #                      lambda e: self.__canvas_scroll.configure(scrollregion=self.__canvas_scroll.bbox("all")))
+        self.__sub_frame.bind("<Configure>",
+                              lambda e: self.__canvas_scroll.configure(
+                                  scrollregion=self.__canvas_scroll.bbox("all")))
 
     def pack_on_scroll(self, widget:tk.Widget, **kwargs):
-        print(self.__canvas_scroll.winfo_width())
         widget.pack(**kwargs)
-        #Actualiza el canvas por cada pack
-        self.__sub_frame.bind("<Configure>",
-                                  lambda e: self.__canvas_scroll.configure(
-                                      scrollregion=self.__canvas_scroll.bbox("all")))
+        # Actualiza el canvas por cada pack
+        self.__canvas_scroll.configure(scrollregion=self.__canvas_scroll.bbox("all"))
 
     @property
     def scr_frame(self):
         return self.__sub_frame
-
-
-
-
--- main.py
+++ main.py
@@ -8,21 +8,33 @@ b = tk.Button(f, text='saluda', command=lambda: f.change_page(f2), cursor='fleur
 b.pack(padx=50, pady=10, side='bottom')
 frame = tk.Frame(f2, bg='#BC9FCF')
 frame.pack()
-tabla = ScrollFrame(f2, width=200, height=300, bg='#7DCC4E')
+tabla = ScrollFrame(f2, width=500, height=300, bg='#7DCC4E')
 tabla.pack()
 
 for i in range(4):
     l2 = tk.Label(tabla.scr_frame, text='HOLA')
     tabla.pack_on_scroll(l2, side='right', anchor='ne')
 
+
+def texto_prueba(texto):
+    global  text_v, l_f3
+    text_v = "Hola "+ str(texto)
+    l_f3.config(text=text_v)
+    f2.change_page(f3)
+
+text_v = tk.StringVar()
 for i in range(20):
-    b2 = tk.Button(tabla.scr_frame, text=f'saluda {i}', command=lambda: print("hola"))
+    b2 = tk.Button(tabla.scr_frame, text=f'saluda {i}', command=lambda: texto_prueba(i))
     tabla.pack_on_scroll(b2)
-
-
-
 
 b = tk.Button(frame, text='volver', command=lambda: f2.change_page(f))
 b.pack(padx=50, pady=10, side='right')
 
+f3 = Page(root, bg='#FFE46E')
+
+l_f3 = tk.Label(f3, text='Hola')
+l_f3.pack()
+
+b_f3 = tk.Button(f3, text='Volver al menú principal', command=lambda:f3.change_page(f))
+b_f3.pack()
 root.mainloop()
