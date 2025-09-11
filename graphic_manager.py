aad1ecfb533f8ec2fdd3a25d4e13b3d178b32263
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-10 21:33:47 -0600

    Modiciaciones en la clase ScrollFrame, arreglo de un problema que permite salir de los marcos establecidos por el canvas dentro del scroll

--- graphic_manager.py
+++ graphic_manager.py
@@ -1,10 +1,8 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import PhotoImage
-
 
 class Window(tk.Tk):
-    def __init__(self, title='ventana', geometry=tuple[int, int], icon_image='', **kwargs):
+    def __init__(self, title='ventana', geometry:tuple[int, int]=(500,500), icon_image='', **kwargs):
         super().__init__(**kwargs)
         self.__principal_page = None
         self.__CURRENT_FRAME = None
@@ -45,22 +43,21 @@ class Window(tk.Tk):
 class Page(tk.Frame):
     # **kwargs para heredar todos los argumentos de tk.Frame
     # https://www.geeksforgeeks.org/python/packing-and-unpacking-arguments-in-python
-    def __init__(self, master, **kwargs):
+    def __init__(self, master:Window, **kwargs):
         super().__init__(master=master, **kwargs)
         self._clear_widgest = []
 
     @property
-    def clear_widget(self):
+    def clear_widgest(self):
         return self._clear_widgest
 
-    @clear_widget.setter
-    def add_clear_widgets(self, widget: list[tk.Widget]):
-        if widget not in self.clear_widget:
-            self.clear_widget.extend(widget)
+    @clear_widgest.setter
+    def clear_widgest(self, widgets: list[tk.Widget]):
+        self.clear_widgest.extend(widgets)
 
     def __clear_widgets_in_frame(self):
         for widget in self._clear_widgest:
-            if isinstance(widget, (tk.Label)):
+            if isinstance(widget, tk.Label):
                 widget.config(text=' ')
             elif isinstance(widget, tk.Entry):
                 widget.delete(0, tk.END)
@@ -86,40 +83,40 @@ class ScrollFrame(tk.Frame):
     def __init__(self, master, vbar_position='right', hbar_position='bottom', **kwargs):
         super().__init__(master=master, **kwargs)
         if int(self.cget('width')) > 0 and int(self.cget('height')) > 0: self.pack_propagate(False)
-        self.__canvas_scroll = tk.Canvas(self)
+        self.__canvas_scroll = tk.Canvas(self, bg= '#ffffff')
+        self.__canvas_scroll.pack_propagate(False)
         # BARRAS PARA MOVER (SE ME OLVIDO COMO SE DICEN EN ESPAÑOL xd)
-        self.__vbar = ttk.Scrollbar(self, orient="vertical")  # VERTICAL BAR
-        self.__vbar.pack(side=vbar_position, fill="y")
+        self.__vbar = tk.Scrollbar(self, orient="vertical")  # VERTICAL BAR
+
         self.__vbar.config(command=self.__canvas_scroll.yview)
-        self.__hbar = ttk.Scrollbar(self, orient="horizontal")  # HORIZONTAL BAR
-        self.__hbar.pack(side=hbar_position, fill="x")
+        self.__hbar = tk.Scrollbar(self, orient="horizontal")  # HORIZONTAL BAR
+
         self.__hbar.config(command=self.__canvas_scroll.xview)
         # HACER QUE EL CANVAS SE MUEVA
         self.__canvas_scroll.config(xscrollcommand=self.__hbar.set, yscrollcommand=self.__vbar.set)
+
+        self.__vbar.pack(side=vbar_position, fill="y")
+        self.__hbar.pack(side=hbar_position, fill="x")
+
         self.__canvas_scroll.pack(side="left", expand=True, fill='both')
         self.__sub_frame = tk.Frame(self.__canvas_scroll)
         # ESTO LO GUARDO NOMAS PARA LIMITAR EL SCROLL (PCHES BOTONES)
         self.__canvas_scroll.create_window((0, 0), window=self.__sub_frame, anchor="nw")
+        #self.__sub_frame.bind("<Configure>",
+        #                      lambda e: self.__canvas_scroll.configure(scrollregion=self.__canvas_scroll.bbox("all")))
+
+    def pack_on_scroll(self, widget:tk.Widget, **kwargs):
+        print(self.__canvas_scroll.winfo_width())
+        widget.pack(**kwargs)
+        #Actualiza el canvas por cada pack
         self.__sub_frame.bind("<Configure>",
-                              lambda e: self.__canvas_scroll.configure(scrollregion=self.__canvas_scroll.bbox("all")))
+                                  lambda e: self.__canvas_scroll.configure(
+                                      scrollregion=self.__canvas_scroll.bbox("all")))
 
     @property
-    def scr_frame(self): return self.__sub_frame
+    def scr_frame(self):
+        return self.__sub_frame
 
 
-root = Window('Ventana', (500, 500))
-f = PagePrincipal(root, bg='#E8CFB0')
-f2 = Page(root, bg='#CEF5EB')
-b = tk.Button(f, text='saluda', command=lambda: f.change_page(f2), cursor='fleur')
-b.pack(padx=50, pady=10, side='bottom')
-frame = tk.Frame(f2, bg='#BC9FCF')
-frame.pack()
-tabla = ScrollFrame(f2, width=200, height=300)
-tabla.pack()
-b2 = tk.Button(tabla.scr_frame, text='saluda', command=lambda: print("hola"), state='disabled')
-b2.pack()
-b = tk.Button(frame, text='volver', command=lambda: f2.change_page(f))
-b.pack(padx=50, pady=10, side='right')
 
-root.mainloop()
 
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Primer commit')
+import tkinter as tk
+from graphic_manager import Window, PagePrincipal, Page, ScrollFrame
+
+root = Window('Ventana', (500, 500))
+f = PagePrincipal(root, bg='#E8CFB0')
+f2 = Page(root, bg='#CEF5EB')
+b = tk.Button(f, text='saluda', command=lambda: f.change_page(f2), cursor='fleur')
+b.pack(padx=50, pady=10, side='bottom')
+frame = tk.Frame(f2, bg='#BC9FCF')
+frame.pack()
+tabla = ScrollFrame(f2, width=200, height=300, bg='#7DCC4E')
+tabla.pack()
+
+for i in range(4):
+    l2 = tk.Label(tabla.scr_frame, text='HOLA')
+    tabla.pack_on_scroll(l2, side='right', anchor='ne')
+
+for i in range(20):
+    b2 = tk.Button(tabla.scr_frame, text=f'saluda {i}', command=lambda: print("hola"))
+    tabla.pack_on_scroll(b2)
+
+
+
+
+b = tk.Button(frame, text='volver', command=lambda: f2.change_page(f))
+b.pack(padx=50, pady=10, side='right')
+
+root.mainloop()
