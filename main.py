6fbfe567d57ded713f3502d0d0a997801ce7a23f
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-13 12:20:31 -0600

    Re organización del sistema en carpetas para tener un mayor orden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from graphic_tools import Window, PagePrincipal, Page, ScrollFrame, Tabla
+from menus.graphic_tools import Window, PagePrincipal, Page, ScrollFrame, Tabla
 
 root = Window('Ventana', (500, 500))
 root.mainloop()
