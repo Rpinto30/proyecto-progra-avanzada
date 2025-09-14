4335dcc0db090623637f55f2111e5340fbda8b16
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-13 18:04:16 -0600

    Prototipo de algunas funcionalidadeds del programa como la carga de archivos actualmente de la computadora

---
+++ data/data_base.py
@@ -0,0 +1,45 @@
+import json
+from os import path
+
+class DataBase:
+    def __init__(self):
+        self.__students = {}
+        self.__instructors = {}
+        self.__courses = {}
+
+        self.__students = self.load_data('students')
+        self.__instructors = self.load_data('instructors')
+        self.__courses = self.load_data('courses')
+
+
+    @property
+    def dict_students(self): return self.__students
+    @dict_students.setter
+    def set_students(self, st): self.__students = st
+    @property
+    def dict_instructors(self): return self.__instructors
+    @property
+    def dict_courses(self): return self.__courses
+
+
+    #Reutilizando parte de la lógica del parcial
+    #https://github.com/Rpinto30/Parcial-Progra-avanzada/blob/main/save_data.py
+    def save_data(self, name_file:str):
+        path_data = path.join('data', f"{name_file}.json")
+        with open(path_data, mode="w", encoding="utf-8") as write_file:
+            if name_file == 'students':
+                json.dump(self.__students, write_file)
+            elif name_file == 'instructors':
+                json.dump(self.__students, write_file)
+            elif name_file == 'courses':
+                json.dump(self.__students, write_file)
+
+    def load_data(self, name_file):
+        try:
+            path_data = path.join('data', f"{name_file}.json")
+            with open(path_data, mode="r", encoding="utf-8") as read_file:
+                return json.load(read_file)
+        except FileNotFoundError:
+            self.save_data(name_file)
+            return {}
+
--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import tkinter as tk
-from menus.graphic_tools import Window, PagePrincipal, Page
 
+from menus.graphic_tools import Window, PagePrincipal, Page
+from data.data_base import DataBase
+from clases_internas.usuarios import Student
+
+#EN EL MAIN SIEMPRE HACER ESTOS DOS
+data = DataBase()
 root = Window('Ventana', (1920, 500))
-#CLASE LOGIN
+#CLASE LOGIN EJEMPLO
 class Login(PagePrincipal):
     def __init__(self, master, **kwargs):
         super().__init__(master=master,bg='red', **kwargs)
-        b = tk.Button(self, text='Sacar toplevel', command=lambda:self.__crear_usuario())
-        b.pack(pady=50)
+        #AQUÍ IRIA LOS BOTONES, FRAMES, TEXTOS DEL LOGIN (los botones de abajo son un ejemplo nada más)
+        b = tk.Button(self, text='Sacar toplevel', command=lambda:self.__crear_usuario()) #<- BOTÓN EJEMPLO
+        b.pack(pady=50) #<- BOTÓN EJEMPLO
         tk.Label(self, text='De quién es el siguiente menú?').pack() #Lo hago directo porque luego no lo uso
         e = tk.Entry(self) #Como si este fuese el de Entry de código en la interfaz
         e.pack()
-        #FUNCIÓN PARA CAMBIAR AL X MENÚ
+        #FUNCIÓN PARA CAMBIAR AL X MENÚ, en vez de instanciar meú de prueba, instanciar el menú que se desea
         def cambiar_a_prueba():
-            m = MenuPrueba(master=self.master, bg='green', who=str(e.get())) #ISNTANCIA DEL MENÚ
-            self.change_page(m) #SE CAMBIA DE PAGINA
-        b_cambio = tk.Button(self, text='Cambiar página', command=lambda:cambiar_a_prueba())
-        b_cambio.pack()
+            m = MenuPrueba(master=self.master, bg='green', who=str(e.get())) #ISNTANCIA DEL MENÚ QUE SE DESEE
+            self.change_page(m) #SE CAMBIA DE PAGINA (OBLIGATORIO)
+
+        b_cambio = tk.Button(self, text='Cambiar página', command=lambda:cambiar_a_prueba()) #<- BOTÓN EJEMPLO
+        b_cambio.pack()#<- BOTÓN EJEMPLO
+
+    #ESTE ES UN EJEMPLO PARA UN TOPLEVEL
     def __crear_usuario(self):
         top_level = tk.Toplevel(self.master)
         top_level.pack_propagate(False) #PARA EVITAR QUE SE DEFORME AL HACER UN PACK
@@ -33,7 +42,32 @@ class MenuPrueba(Page): #CLASE DE MENÚ DE PRUEBA
         def close_menu():
             self.change_page(master.principal_page)
         #para volver al login (la unica que va a perdurar) se cambia a lo que guardo la root(aquí master) en login_page
+
+        def add_st_test():
+            if ID.get() not in data.dict_students:
+                st = Student(ID.get(), nombre.get(),contra.get(), '')
+                print(st.user_id)
+                data.set_students[str(ID.get())] = {'name': nombre.get(), 'contra':contra.get()}
+                data.save_data('students')
+            else: print("No")
+        tk.Label(self, text='ID:').pack()
+        ID = tk.Entry(self)
+        ID.pack()
+
+        tk.Label(self, text='Nombre:').pack()
+        nombre = tk.Entry(self)
+        nombre.pack()
+
+        tk.Label(self, text='Contraseña:').pack(pady=10)
+        contra = tk.Entry(self)
+        contra.pack()
+
+        tk.Button(self, text='Añadir algo a estudiantes', command=lambda:add_st_test()).pack(pady=20)
+        tk.Button(self, text='Probar base de datos', command=lambda:print(data.dict_students)).pack(pady=20)
+
         tk.Button(self,text='Regresar', command= lambda: close_menu()).pack()
+
+
 
 login = Login(root) #Solo vamos a reutilizar el login, por eso se coloca como atributo del root
 root.mainloop()
