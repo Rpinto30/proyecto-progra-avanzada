ecfbcd2a64a7f75d08e8890d9015e2820700cfef
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-15 21:29:46 -0600

    Creacion studenMenu

--- menus/student_menu.py
+++ menus/student_menu.py
@@ -1,5 +1,13 @@
 from graphic_tools import Window, PagePrincipal
 
+root = Window('Test', (1920,1080))
+
+COLOR_BG = '#669BBC'
+
 class StudentMenu(PagePrincipal):
     def __init__(self, master:Window, **kwargs):
-        super().__init__(master,**kwargs)
+        super().__init__(master, bg=COLOR_BG,**kwargs)
+
+S = StudentMenu(root)
+
+root.mainloop()
