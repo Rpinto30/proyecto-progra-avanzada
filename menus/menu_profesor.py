994d51a40f0aee4217df56a2c0adc830cba93c42
Aileen127 <fleuraileen394@gmail.com>
2025-09-16 23:11:08 -0600

    Cambios de ubicación de los frames de información

--- menus/menu_profesor.py
+++ menus/menu_profesor.py
@@ -26,10 +26,17 @@ class MenuProfesores(PagePrincipal):
         self.__barra_profesor = tk.Frame(self, width=700, height=1080, bg=azul_claro)
         self.__barra_profesor.pack_propagate(False)
         self.__barra_profesor.pack(side='left')
-        self.__frame_profesor = tk.Frame(self.__barra_profesor, width=1920, height=1080, bg=blanco_hueso)
+        self.__frame_profesor = tk.Frame(self, width=1920-700, height=1080, bg=blanco_hueso)
         self.__frame_profesor.pack_propagate(False)
-        self.__frame_profesor.pack(pady=10)
+        self.__frame_profesor.pack(side='right')
 
+        frame__colocar_info = tk.Frame(self.__barra_profesor, bg='black')
+        frame__colocar_info.pack(pady=10)
+        tk.Label(frame__colocar_info, text='Profesor', font=(font, 60, 'bold'), anchor='w', bg='black', fg=fg).pack(padx=35,
+                                                                                                               fill='x',
+                                                                                                               pady=10)
+        tk.Label(frame__colocar_info, text=f'{user.name} ({user.user_id})', font=(font, 25, 'bold'), anchor='w', bg='black',
+                 fg=fg).pack(padx=35, fill='x')
 
         self.__barra_materia_boton = tk.Button(self.__barra_profesor, text="Curso", bg=azul_marino, fg=fg, font=font)
         self.__barra_materia_boton.pack(fill="x", pady='250')
@@ -39,10 +46,6 @@ class MenuProfesores(PagePrincipal):
         self.__frame_info.pack()
 
         # frame colocar info
-        frame__colocar = tk.Frame(self.__frame_info, bg='black')
-        frame__colocar.pack(pady=10)
-        tk.Label(frame__colocar, text= 'Profesor' , font=(font, 60, 'bold'), anchor='w', bg='black', fg=fg).pack(padx=35, fill='x', pady=10)
-        tk.Label(frame__colocar, text=f'{user.name} ({user.user_id})', font=(font, 25, 'bold'), anchor='w', bg='black', fg=fg).pack(padx=35, fill='x')
 
 
 
